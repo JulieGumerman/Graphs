c3233809d3fbff986edd49f33503b7c7ff6b6db4
bodhibrdr <julie.gumerman@gmail.com>
2020-03-16 19:04:20 -0600

    well, that's not going to work...

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,17 @@
+from util import Stack, Queue
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+        s = Stack()
+        visited = set()
+
+        s.push(starting_node)
+        while s.size() > 0:
+            current_node = s.pop()
+
+            if current_node not in visited:
+                visited.add(current_node)
+                print(current_node)
+                edges = self.get_neighbors(current_node)
+                for e in edges:
+                    s.push(e)
