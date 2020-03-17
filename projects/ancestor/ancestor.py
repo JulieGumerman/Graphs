2c150992a475277e3abd77b2fc10fad54e7f4069
bodhibrdr <julie.gumerman@gmail.com>
2020-03-17 15:07:47 -0600

    and now we're making progress...

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,47 +1,49 @@
 from util import Stack, Queue
 
-def get_neighbors():
-    pass
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+    
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist")
+
+    def get_neighbors(self, vertex_id):
+        return self.vertices[vertex_id]
     
 
 def earliest_ancestor(ancestors, starting_node):
-    # queue = Queue()
-    # visited = set()
-    # queue.enqueue([starting_node])
+    graph = Graph()
 
-    # while queue.size > 0:
-    #     current_path = queue.dequeue()
-    #     current_node = current_path[-1]
-
-    #     if current_node not in visited:
-    #         visited.add(current_node)
-
-    #         edges = getNeighbors(current_node)
-
-    #         for edge in edges:
-    #             path_copy = list(current_path)
-    #             path_copy.append(edge)
-    #             queue.enqueue(path.copy)
-    # return path_copy[-1]
-    path = []
-    distant_ancestor = None
-    for item in ancestors:
-        if item[0] == starting_node:
-            distant_ancestor = item[1]
+    for node in ancestors:
+        graph.add_vertex(node[0])
+        graph.add_vertex(node[1])
+        graph.add_edge(node[1], node[0])
     
-    for next_ancestor in ancestors:
-        if next_ancestor[0] == distant_ancestor:
-            distant_ancestor = next_ancestor[1]
+    q = Queue()
+    q.enqueue([starting_node])
 
-    # for farther_back in ancestors:
-    #     if farther_back[0] == distant_ancestor:
-    #         distant_ancestor = farther_back[1]
-    
-    # for prev_gen in ancestors:
-    #     if prev_gen[0] == distant_ancestor:
-    #         distant_ancestor = prev_gen[1]
-    
-    return distant_ancestor
+    earliest_ancestor = None
+    longest_path = []
+    visited = set()
 
-    #return ancestors
+    while q.size() > 0:
+        path = q.dequeue()
+        current_vertex = path[-1]
 
+        if len(path) > len(longest_path):
+            earliest_ancestor = current_vertex
+            longest_path = path
+        if current_vertex not in visited:
+            for neighbor in graph.get_neighbors(current_vertex):
+                path_copy = path.copy()
+                path_copy.append(neighbor)
+                q.enqueue(path_copy)
+        
+
+    return earliest_ancestor
